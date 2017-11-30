b98d995211c7ca379121f9b4c4ca203bf6561dcd
MocinGejbr <gabor.martin1212@gmail.com>
2017-11-30 06:51:41 +0100

    First version SWIFT 0-100 app

--- ZeroToAHundred/ViewController.swift
+++ ZeroToAHundred/ViewController.swift
@@ -8,16 +8,43 @@
 
 import UIKit
 
-class ViewController: UIViewController {
+class ViewController: UIViewController, UITextFieldDelegate {
+    
+    //MARK: Properties
+    
+    @IBOutlet weak var nameLabel: UILabel!
+    @IBOutlet weak var nameTextField: UITextField!
+    
 
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
+        nameTextField.delegate = self
     }
 
     override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
         // Dispose of any resources that can be recreated.
+    }
+    //MARK: UITextFieldDelegate
+    
+    func textFieldShouldReturn(_ textField: UITextField) -> Bool {
+        //Hide keyboard
+        textField.resignFirstResponder()
+        return true
+    }
+    
+    func textFieldDidEndEditing(_ textField: UITextField) {
+        nameLabel.text = textField.text
+        textField.text = ""
+        textField.placeholder = "Set name"
+        }
+
+    
+    //MARK: Actions
+
+    @IBAction func setDefaultName(_ sender: UIButton) {
+        nameLabel.text = "Martin Gabor"
     }
 
 
